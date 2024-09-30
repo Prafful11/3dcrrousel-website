e68f63d9d18a8afa63ea8a6060b7fbb4939a06e4
Prafful11 <154907918+Prafful11@users.noreply.github.com>
2024-09-30 19:13:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var imgWidth = 120; // width of images (unit: px)
 var imgHeight = 170; // height of images (unit: px)
 
 // Link of background music - set 'null' if you dont want to play background music
-var bgMusicURL = '';
+var bgMusicURL = 'null';
 var bgMusicControls = true; // Show UI music control
 
 
